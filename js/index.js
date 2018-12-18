29bc94b3443a64993567f174f41a8b265d827b36
Jinnan Zhang <vannizhang@gmail.com>
2018-12-17 16:02:29 -0800

    add blend style layers

--- js/index.js
+++ js/index.js
@@ -1,152 +1,80 @@
 require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/widgets/LayerList",
     "esri/layers/TileLayer",
     "esri/layers/VectorTileLayer",
     "esri/layers/BaseTileLayer",
     "esri/core/promiseUtils"
 ],
 function(
-    Map, MapView, LayerList, TileLayer, VectorTileLayer,
+    Map, MapView, TileLayer, VectorTileLayer,
     BaseTileLayer, promiseUtils
 ){
 
-    // // **********************************************
-    // //  Create a subclass of BaseTileLayer
-    // // **********************************************
-
-    // var BlendLayer = BaseTileLayer.createSubclass({
-
-    //     // Add a multiplyLayers property. Its value will be used
-    //     // store tile layers that will be used to create
-    //     // a blended layer. Layers stored in this property
-    //     // will be blended using "multiply" operation.
-    //     properties: {
-    //         multiplyLayers: null,
-    //         blendMode: null
-    //     },
-
-    //     // The load() method is called when the layer is added to the map
-    //     // prior to it being rendered in the view.
-    //     load: function() {
-    //         // call load method on each tile layer stored in multiple property
-    //         this.multiplyLayers.forEach(function(layer) {
-    //             // The tile layers must load() prior to the BlendLayer
-    //             // resolving and moving to the "loaded" status.
-    //             this.addResolvingPromise(layer.load());
-    //         }, this);
-    //     },
-
-    //     // Fetches the tile(s) visible in the view
-    //     fetchTile: function(level, row, col) {
-
-    //         var tilePromises = this.multiplyLayers.map(function(layer) {
-    //             // calls fetchTile() on the tile layers returned in multiplyLayers property
-    //             // for the tiles visible in the view
-    //             // console.log(layer);
-    //             return layer.fetchTile(level, row, col);
-    //         });
-
-    //         return promiseUtils.eachAlways(tilePromises).then(function(results) {
-        
-    //             // create a canvas
-    //             var width = this.tileInfo.size[0];
-    //             var height = this.tileInfo.size[0];
-    //             var canvas = document.createElement("canvas");
-    //             var context = canvas.getContext("2d");
-
-    //             canvas.width = width;
-    //             canvas.height = height;
-
-    //             // context.globalCompositeOperation = "luminosity";
-    //             context.globalCompositeOperation = this.blendMode || 'normal';
-
-    //             // console.log(this.blendMode);
-
-    //             // const layers = this.multiplyLayers;
-            
-    //             results.forEach(function(result, index) {
-    //                 var image = result.value;
-
-    //                 const cssFilter = this.multiplyLayers[index].cssFilter || null;
-
-    //                 context.filter = cssFilter || 'none';
-
-    //                 // console.log(image);
-
-    //                 context.drawImage(image, 0, 0, width, height);
-    //             }, this);
-
-    //             return canvas;
-    //         }.bind(this));
-    //     }
-    // });
-
-    // // **********************************************
-    // // Start of JS application
-    // // **********************************************
-
-    // // natGeoLayer and hillShadeLayer are added to
-    // // multiplyLayers property of the BlendLayer
-
-    // var imageryLayer = new TileLayer({
-    //     url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
-    //     // cssFilter: 'brightness(1.3) hue-rotate(-45deg) contrast(5) saturate(0.5)'
-    // });
-
-    // var hillShadeLayer = new TileLayer({
-    //     url: "https://services.arcgisonline.com/arcgis/rest/services/Elevation/World_Hillshade/MapServer",
-    //     // cssFilter: 'brightness(0.9) contrast(1.15)'
-    // });
-
-    // // Create a new instance of BlendLayer
-    // // add natGeoLayer and hillShadeLayer to multiplyLayers property
-    // var blendLayer = new BlendLayer({
-    //     multiplyLayers: [
-    //         imageryLayer,
-    //         hillShadeLayer
-    //     ],
-    //     title: "Blended World Imagery Map",
-    //     // blendMode: 'luminosity' 
-    //     blendMode: 'color-burn' 
-    // });
-
-    // // Add natGeoLayer and blendLayer to the map
-    // var map = new Map({
-    //     layers: [
-    //         blendLayer
-    //     ]
-    // });
-
-    // var view = new MapView({
-    //     container: "viewDiv",
-    //     map: map,
-    //     center: [-117.0431, 34.0336],
-    //     zoom: 12,
-    //     constraints: {
-    //         snapToZoom: false
-    //     }
-    // });
-
-    const Controller = function(){
-
+    const BLEND_MODE_RECIPE = {
+        "Chiffon": {
+            blendLayerOptions: {
+                blendMode: "luminosity",
+                imageryLayerCssFilter: "brightness(1.3) hue-rotate(-45deg) contrast(5) saturate(0.5)",
+                hillShadeLayerCssFilter: "brightness(0.9) contrast(1.15)"
+            },
+            labelLayerOptions: {
+                itemID: "ba52238d338745b1a355407ec9df6768",
+                // modifierClass: "Human-Geography-Label-Chiffon",
+                opacity: .75
+            },
+            deatiledLayerOptions: {
+                itemID: "97fa1365da1e43eabb90d0364326bc2d",
+                // modifierClass: "Human-Geography-Detail-Chiffon",
+                opacity: .25
+            }
+        },
+        "Bumpify": {
+            blendLayerOptions: {
+                blendMode: "color-burn",
+                imageryLayerCssFilter: "",
+                hillShadeLayerCssFilter: ""
+            },
+            labelLayerOptions: {
+                itemID: "4a3922d6d15f405d8c2b7a448a7fbad2",
+                // modifierClass: "Human-Geography-Label-Dark",
+                opacity: .5
+            },
+            deatiledLayerOptions: {
+                itemID: "1ddbb25aa29c4811aaadd94de469856a",
+                // modifierClass: "Human-Geography-Detail-Dark",
+                opacity: .25
+            }
+        },
+        "War Room": {
+            blendLayerOptions: {
+                blendMode: "luminosity",
+                imageryLayerCssFilter: ": brightness(1.25) saturate(0.3) hue-rotate(-25deg)",
+                hillShadeLayerCssFilter: "invert(0.9)"
+            },
+            labelLayerOptions: {
+                itemID: "4a3922d6d15f405d8c2b7a448a7fbad2",
+                // modifierClass: "Human-Geography-Label-Dark",
+                opacity: .5
+            }
+        }
     };
-    
+
     const MapControl = function(){
 
         let mapView = null;
 
         const DOM_ID_MAP_CONTAINER = 'viewDiv';
-        const CENTER_COORD = [-117.0431, 34.0336];
-        const ZOOM = 12;
+        const CENTER_COORD = [-95, 40];
+        const ZOOM = 5;
 
-        const init = ()=>{
+
+        const init = (options={})=>{
             initMapView();
 
-            addBlendLayer({
-                styleName: 'War Room'
-            });
+            if(options.styleRecipe){
+                addBlendLayer(options.styleRecipe);
+            }
         };
 
         const initMapView = ()=>{
@@ -195,71 +123,34 @@ function(
             return blendLayer;
         };
 
-        const getLabelLayer = (options={})=>{
+        const getVectorTileLayer = (options={})=>{
 
-            const blendMode = options.blendMode || 'normal';
-            
-            // const darkLayer = new TileLayer({
-            //     url: "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer"
-            // });
-            
-            // const blendLabelLayer = new BlendLayer({
-            //     multiplyLayers: [
-            //         darkLayer
-            //     ],
-            //     blendMode: blendMode 
-            // });
+            const itemID = options.itemID || null;
 
             const vtlLayer = new VectorTileLayer({
-                id: 'darkLabelLayer',
-                // visible: false,
-                opacity: .75,
-                url: "https://www.arcgis.com/sharing/rest/content/items/4a3922d6d15f405d8c2b7a448a7fbad2/resources/styles/root.json?f=pjson"
+                portalItem: {  // autocasts as new PortalItem()
+                    id: itemID
+                },
+                opacity: options.opacity || 1,
             });
               
             return vtlLayer;
 
         };
 
-        const addBlendLayer = (options={})=>{
+        const addBlendLayer = (styleRecipe={})=>{
 
-            const styleName = options.styleName || "Chiffon";
+            const blendLayer = getBlendLayer(styleRecipe.blendLayerOptions);
 
-            const recipes = {
-                "Chiffon": {
-                    blendLayerOptions: {
-                        blendMode: "luminosity",
-                        imageryLayerCssFilter: "brightness(1.3) hue-rotate(-45deg) contrast(5) saturate(0.5)",
-                        hillShadeLayerCssFilter: "brightness(0.9) contrast(1.15)"
-                    }
-                },
-                "Bumpify": {
-                    blendLayerOptions: {
-                        blendMode: "color-burn",
-                        imageryLayerCssFilter: "",
-                        hillShadeLayerCssFilter: ""
-                    },
-                    labelLayerOptions: {
-                        blendMode: "soft-light",
-                    }
-                },
-                "War Room": {
-                    blendLayerOptions: {
-                        blendMode: "luminosity",
-                        imageryLayerCssFilter: ": brightness(1.25) saturate(0.3) hue-rotate(-25deg)",
-                        hillShadeLayerCssFilter: "invert(0.9)"
-                    },
-                    labelLayerOptions: {
-                        blendMode: "soft-light",
-                    }
-                }
-            };
+            const labelLayer = styleRecipe.labelLayerOptions ? getVectorTileLayer(styleRecipe.labelLayerOptions) : null;
 
-            const blendLayer = getBlendLayer(recipes[styleName].blendLayerOptions);
+            const deatiledLayer = styleRecipe.deatiledLayerOptions ? getVectorTileLayer(styleRecipe.deatiledLayerOptions) : null;
 
-            // const labelLayer = recipes[styleName].labelLayerOptions ? getLabelLayer(recipes[styleName].labelLayerOptions) : null;
+            const layersToAdd = [blendLayer, deatiledLayer, labelLayer].filter(d=>d !== null);
 
-            mapView.map.layers.addMany([blendLayer]);
+            mapView.map.layers.removeAll();
+
+            mapView.map.layers.addMany(layersToAdd);
 
         };
 
@@ -318,15 +209,79 @@ function(
 
     };
 
-    const View = function(){
+    const CustomizedLayerList = function(options={}){
 
+        const container = options.container ? document.getElementById(options.container) : null;
+        const layers = options.layers || [];
+        const layerOnSelectHandler = options.layerOnSelectHandler || null;
+
+        const init = ()=>{
+
+            if(!container){
+                console.error('container is required to initiate layer list');
+                return;
+            }
+
+            render();
+            addEventHandler();
+        };
+
+        const render = ()=>{
+
+            const layersHtml = layers.map( (d, i)=>{
+                const isActive = i === 0 ? 'is-active' : '';
+                return `<a class="layer-list-link crumb link-white js-set-blend-layer ${isActive}" data-value='${d}'>${d}</a>`;
+            }).join('');
+
+            const layerListHtml = `<div class='panel panel-black'><nav class="breadcrumbs">${layersHtml}</nav><div>`;
+
+            container.innerHTML = layerListHtml;
+        };
+
+        const addEventHandler = ()=>{
+            container.addEventListener('click', function (event){
+                if (event.target.classList.contains('js-set-blend-layer')) {
+                    const targetLayer = event.target.dataset.value;
+                    layerOnSelectHandler(targetLayer);
+                    setActiveItem(targetLayer);
+                }
+            })
+        };
+
+        const setActiveItem = (layerName)=>{
+            document.querySelectorAll('.layer-list-link').forEach(d=>{
+                if(d.dataset.value === layerName){
+                    d.classList.add('is-active');
+                } else {
+                    d.classList.remove('is-active');
+                }
+            });
+        };
+
+        init();
     };
 
     const initApp = function(){
-        const controller = new Controller();
+
+        const availableStyleRecipes = Object.keys(BLEND_MODE_RECIPE);
+
+        const defaultStyleRecipe = BLEND_MODE_RECIPE[availableStyleRecipes[0]];
+        
         const mapControl = new MapControl();
 
-        mapControl.init();
+        const layerList = new CustomizedLayerList({
+            container: 'layerListDiv',
+            layers: availableStyleRecipes,
+            layerOnSelectHandler: (layerName)=>{
+                const styleRecipe = BLEND_MODE_RECIPE[layerName];
+                mapControl.addBlendLayer(styleRecipe);
+            }
+        });
+
+        mapControl.init({
+            styleRecipe: defaultStyleRecipe
+        });
+
     };
 
     initApp();
